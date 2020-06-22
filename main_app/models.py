479f633478e40608a3a40319753d0db7feff1407
MJMoquin <mjmoquin@gmail.com>
2020-06-22 15:08:00 -0500

    fix signup

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     ingredients = models.TextField(max_length=250)
     instructions = models.TextField(max_length=900)
 
+class Reviews(models.Model):
+    rating = models.IntegerField()
+    comment = models.TextField(max_length=250)
+    modifications = models.TextField(max_length=250)
