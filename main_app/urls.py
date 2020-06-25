f757ef13716d7dd84ed7e90d65d04950bad729f3
MJMoquin <mjmoquin@gmail.com>
2020-06-25 16:44:04 -0500

    fix signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/', views.recipes_index, name='recipes_index'),
-    path('results/', views.search_results, name='search_results'),
-    path('recipes/<int:recipe_id>/', views.recipe_detail, name='detail'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('recipes/', views.recipes_index, name='recipes_index'),
     path('recipes/create/', views.RecipeCreate.as_view(), name='recipe_create'),
+    path('recipes/<int:recipe_id>/', views.recipe_detail, name='detail'),
     path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name='recipe_update'),
     path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipe_delete'),
     path('recipes/<int:recipe_id>/add_review/', views.add_review, name='add_review'),
